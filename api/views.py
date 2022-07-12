a8e2500be55446794520171d4b6455adf469e399
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-07-12 22:43:24 +0600

    rest framework added successfully

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_status(request):
+    return Response(
+        {
+            "status": 200,
+            "message": "yes! django is working"
+        }
+    )
--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
