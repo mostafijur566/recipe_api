33c8f974d8d9481b84a3e3a0cc1b12fd233826f6
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-07-12 22:37:33 +0600

    recommededRecipe model added, image file upload functionality added

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Recipes(models.Model):
+class Recipe(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(null=False, blank=False)
     cook_time = models.IntegerField()
@@ -11,3 +11,21 @@ class Recipes(models.Model):
     preparation = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name[0:50]
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class RecommendedRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.recipe.name[0:50]
+
+    class Meta:
+        ordering = ['-updated_at']
--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
